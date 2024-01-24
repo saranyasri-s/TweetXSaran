016d4086aa08415f1fdfe73cde9bfdbfd95d7ee3
saranyasri-s <dentist.apassionateprogrammer@gmail.com>
2024-01-24 17:10:03 +0530

    added input for writing newpost

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./Feed.module.css";
+import SingleFeed from "./SingleFeed/SingleFeed";
 function Feed() {
-  return <div className={classes.Feed}>Feed</div>;
+  const [createNewPost, setCreateNewPost] = useState(false);
+  const [newPost, setNewPost] = useState("");
+
+  const handlePostChange = (e) => {
+    e.preventDefault();
+    setNewPost(e.target.value);
+  };
+  const submitnewPost = (e) => {
+    e.preventDefault();
+    if (newPost.length) {
+      // i will  enter code here to put request
+    }
+    setCreateNewPost(false);
+  };
+  return (
+    <div className={classes.Feed}>
+      <button
+        onClick={() => {
+          setCreateNewPost(true);
+        }}
+        className={classes.write}
+      >
+        Write
+      </button>
+      {createNewPost && (
+        <div className={classes.newPost}>
+          <textarea
+            className={classes.NewPostInput}
+            type="text"
+            value={newPost}
+            name="newPost"
+            onChange={handlePostChange}
+            maxLength={100}
+          ></textarea>
+          <button className={classes.postButton} onClick={submitnewPost}>
+            Post
+          </button>
+        </div>
+      )}{" "}
+      <SingleFeed></SingleFeed>
+      <SingleFeed></SingleFeed>
+      <SingleFeed></SingleFeed>
+    </div>
+  );
 }
 
 export default Feed;
---
+++ src/components/Feed/SingleFeed/SingleFeed.js
@@ -0,0 +1,7 @@
+import React from "react";
+import classes from "./SingleFeed.module.css";
+function SingleFeed() {
+  return <div className={classes.SingleFeed}>SingleFeed</div>;
+}
+
+export default SingleFeed;
