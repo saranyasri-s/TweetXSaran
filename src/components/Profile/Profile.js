f867191a45eb9ddf1227bea257d0ed50a7d3fcc0
saranyasri-s <dentist.apassionateprogrammer@gmail.com>
2024-01-24 20:43:09 +0530

    added profilepage info

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,9 +1,22 @@
-import React from 'react'
-import classes from "./Profile.module.css"
+import React from "react";
+import classes from "./Profile.module.css";
 function Profile() {
   return (
-    <div className={classes.Profile}>Profile</div>
-  )
+    <div className={classes.Profile}>
+      <div className={classes.top}>
+        <img className={classes.img}></img>
+        <div className={classes.nameFollowing}>
+          <p className={classes.name}>Arjun Reddy</p>
+          <div className={classes.top_Bottom}>
+            <p className={classes.following}>Posts : 300</p>
+            <p className={classes.following}>Followers : 300</p>
+
+            <p className={classes.following}>Following : 300</p>
+          </div>
+        </div>{" "}
+      </div>
+    </div>
+  );
 }
 
-export default Profile
+export default Profile;
