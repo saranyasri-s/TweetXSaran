dcc0b6fa13e6db855213fc1906d76e4160979237
saranyasri-s <dentist.apassionateprogrammer@gmail.com>
2024-01-24 19:21:51 +0530

    added style for feeds with responsiveness

--- src/components/Feed/SingleFeed/SingleFeed.js
+++ src/components/Feed/SingleFeed/SingleFeed.js
@@ -1,7 +1,31 @@
 import React from "react";
 import classes from "./SingleFeed.module.css";
 function SingleFeed() {
-  return <div className={classes.SingleFeed}>SingleFeed</div>;
+  return (
+    <div className={classes.SingleFeed}>
+      <div className={classes.upLowdiv}>
+        <div className={classes.left}>
+          <img className={classes.img}></img>
+          <p className={classes.name}>Arjun Reddy</p>
+        </div>
+        <p className={classes.time}>10 mins ago</p>
+      </div>
+      <div className={classes.upLowdiv}>
+        <div className={classes.left}>
+          <div className={classes.empty}></div>
+          <p
+            className={classes.post}
+            style={{ marginLeft: "1rem", color: "grey", paddingLeft: "2rem" }}
+          >
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500st
+          </p>
+        </div>
+        <div className={classes.halfRound}> </div>
+      </div>
+    </div>
+  );
 }
 
 export default SingleFeed;
