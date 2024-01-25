795cc42d320f7bd83d0394346b038c5422fda001
saranyasri-s <dentist.apassionateprogrammer@gmail.com>
2024-01-25 17:08:34 +0530

    added functionlity to update following

--- src/components/Users/User.js
+++ src/components/Users/User.js
@@ -1,6 +1,45 @@
 import React from "react";
 import classes from "./User.module.css";
-function User({user}) {
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+import axios from "axios";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../../src/firebase"; // Adjust the path to your firebase.js file
+import { useSelector, useDispatch } from "react-redux";
+import { setUsers } from "../../store/UsersSlice";
+import { setUser } from "../../store/UserSlice";
+
+function User({ user }) {
+  const users = useSelector((state) => state.users);
+  const dispatch = useDispatch();
+  const targetUid = useSelector((state) => state.user.uid);
+
+  const userLogged = useSelector((state) => state.user);
+  console.log(userLogged);
+  const handleUpdateUser = async () => {
+    try {
+      if (userLogged) {
+        const userDocRef = doc(db, "users", userLogged.id);
+
+        // Update the user information
+        await updateDoc(userDocRef, {
+          displayName: "New Display Name-saran", // Update with the new display name
+          following: [...userLogged.following, user.id], // Add other fields you want to update
+        });
+        dispatch(
+          setUser({
+            ...userLogged,
+            following: [...userLogged.following, user.id],
+          })
+        );
+        console.log("User updated successfully");
+      } else {
+        console.log("User not found");
+      }
+    } catch (error) {
+      console.error("Error updating user:", error.message);
+    }
+  };
+
   return (
     <div className={classes.User}>
       <div className={classes.left}>
@@ -12,8 +51,19 @@ function User({user}) {
           </p>
         </div>
       </div>
+      {userLogged.following.includes(user.id) ? (
+        <p
+          className={classes.followingStatus}
+          style={{ fontWeight: "600", color: "grey" }}
+        >
+          Following
+        </p>
+      ) : (
+        <button onClick={handleUpdateUser} className={classes.button}>
+          Follow
+        </button>
+      )}
       {/* <p className={classes.followingStatus} style={{ fontWeight: "600", color: "grey" }}>Following</p> */}
-      <button className={classes.button}>Follow</button>
     </div>
   );
 }
